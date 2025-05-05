6a10c33102ff1b0664ae541301699077690d0e8a
Hariprasad K J <75715861+harip89@users.noreply.github.com>
2025-05-05 15:43:30 +0530

    Update motor_handler.cpp

--- motor_handler.cpp
+++ motor_handler.cpp
@@ -3,15 +3,17 @@
 #include "config.h"
 #include "telegram_handler.h"
 
-String lastStatus = "";
+String lastStatus = "OFF";
 
-String fetchStatus() {
+String fetchStatus()
+{
   HTTPClient http;
   http.begin(String(FIREBASE_URL) + FIREBASE_STATUS_PATH);
   int httpCode = http.GET();
   String result = "";
 
-  if (httpCode == 200) {
+  if (httpCode == 200)
+  {
     result = http.getString();
     result.trim();
     result.replace("\"", "");
@@ -21,15 +23,19 @@ String fetchStatus() {
   return result;
 }
 
-void setMotorState(const String& state) {
+void setMotorState(const String &state)
+{
   int pinState = (state.equalsIgnoreCase("ON")) ? LOW : HIGH;
   digitalWrite(MOTOR_LINE_A, pinState);
   digitalWrite(MOTOR_LINE_B, pinState);
 }
 
-void readAndUpdateMotorStatus() {
+void readAndUpdateMotorStatus()
+{
   String currentStatus = fetchStatus();
-  if (currentStatus != "" && currentStatus != lastStatus) {
+  log("testing this");
+  if (currentStatus != "" && currentStatus != lastStatus)
+  {
     lastStatus = currentStatus;
     setMotorState(currentStatus);
     log("➡️ Polled: " + currentStatus);
